456948feaad3f840327ab557b7dbf65cc51e9913
ern0ld <epa.suvila@gmail.com>
2020-01-22 11:23:18 +0200

    lisätty app.js

--- app.js
+++ app.js
@@ -62,14 +62,11 @@ function bounceCheck(){
     }
 }
 function newPoint(){
-  
+  ctx.beginPath();
     ctx.fillStyle = 'rgba(250,0,0,0.4)';
-    ctx.fillRect(Math.random * can.width, Math.random * can.height , 50, 50);
+    ctx.fillRect(Math.random * 100, Math.random * 100 , 50, 50);
     ctx.fill();
-    ctx.beginPath();
-    ctx.arc(300, 300, 20, 0, 2 * Math.PI);
-    ctx.fillStyle = 'rgba(250,0,0,0.4)';
-    ctx.fill();
+    
     
     
   
