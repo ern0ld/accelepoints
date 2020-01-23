50b4683343a6d489be08f776c8f66463b2f0413f
ern0ld <epa.suvila@gmail.com>
2020-01-23 19:25:51 +0200

    test

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ accelerometer.addEventListener('reading', e => {
     //console.log("Acceleration along the Z-axis " + accelerometer.z);
    
     xvelocity = xvelocity + accelerometer.x;
-    yvelocity = yvelocity + accelerometer.y;
+    yvelocity = yvelocity - accelerometer.y;
   
   
   });
